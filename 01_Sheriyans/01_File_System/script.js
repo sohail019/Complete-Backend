0871ab1267d7f3b7122ed18fbf94504c79ab9a99
sohail019 <shaikhhsohail0193@gmail.com>
2025-04-04 11:52:39 +0530

    Replace hello.txt with greet.txt and update script.js to append and rename files

--- 01_Sheriyans/01_File_System/script.js
+++ 01_Sheriyans/01_File_System/script.js
@@ -6,3 +6,15 @@ fs.writeFile("hello.txt", "Hello World!", (err) => {
   if (err) console.log(err);
   else console.log("File created successfully!");
 });
+
+//? Append file - where we add data to the end of the file
+fs.appendFile("hello.txt", " How are you?", (err) => {
+  if (err) console.log(err);
+  else console.log("File appended successfully!");
+});
+
+//? Rename file - where we change the name of the file
+fs.rename("hello.txt", "greet.txt", (err) => {
+  if (err) console.log(err);
+  else console.log("File renamed successfully!");
+});
