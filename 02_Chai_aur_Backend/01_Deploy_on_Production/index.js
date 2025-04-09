74638c67d7082ee3a60d9cf113ade68b48dd1583
sohail019 <shaikhhsohail0193@gmail.com>
2025-04-09 22:35:55 +0530

    Add package.json for production deployment setup with Express dependency

---
+++ 02_Chai_aur_Backend/01_Deploy_on_Production/index.js
@@ -0,0 +1,28 @@
+//? Import express from express
+const express = require("express");
+
+//? Initialize express in app variable instance
+const app = express();
+
+//? Initialize port (total ports are 65535 we can choose any)
+const port = 4000;
+
+//? let's get 1st response in /
+app.get("/", (req, res) => {
+  res.send("Hello NodeJS");
+});
+
+//? another route
+app.get("/instagram", (req, res) => {
+  res.send("My Instagram username is scookiehail");
+});
+
+//? login route with html
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login here</h1>");
+});
+
+//? listen to the port 4000
+app.listen(port, () => {
+  console.log(`This app is listening to port: ${port}`);
+});
